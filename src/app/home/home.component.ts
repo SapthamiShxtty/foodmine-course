36b80440189dd031c0b2f67031f6fdc7812d27c2
sapthami-devappsys <sapthami@devappsys.com>
2025-05-23 18:09:16 +0530

    Adding search bar - inprogress

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { CommonModule } from '@angular/common';
 import { Food } from '../shared/models/Food';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +14,16 @@ import { Food } from '../shared/models/Food';
 export class HomeComponent implements OnInit {
 
   foods:Food[] =[];
-  constructor(private foodService : FoodService){}
+  constructor(private foodService : FoodService, private route : ActivatedRoute){}
 
   ngOnInit(): void {
-      this.foods = this.foodService.getAll();
+      this.route.params.subscribe(params =>{
+          if(params['searchTerm'])
+            this.foods = this.foodService.getAll().filter(food => food.name.toLowerCase().includes(params['searchTerm'].toLowerCase()));
+          else
+          this.foods = this.foodService.getAll();
+      })
+      
   }
 
   getStarsArray(stars: number): number[] {
